2904ace251ac19c5fa2450c7b8cef86b58aad2bd
Shweta Gokhale <shwetagokhale123@gmail.com>
2025-11-07 11:33:57 +0530

    Aluminium Extrusions

--- current_production/urls.py
+++ current_production/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CurrentProductionView, SensorDetailView
 
 urlpatterns = [
-    #_________________Current Production________________
+    #_________________Current Production.________________
     path('', CurrentProductionView.as_view(), name='current_production'),
     path('sensor-details/', SensorDetailView.as_view(), name='sensor_details'),
 ]
